acd7541cdfcdcb0595f28adaa0ea07090d4fd98b
verdeverdad <sofia.16d@gmail.com>
2025-01-15 22:06:43 -0300

    ejercicio 8

---
+++ Clases/ej8.js
@@ -0,0 +1,9 @@
+const nombre = "Sofía";
+const apellido = "Delmonte";
+const edad = 33;
+const fechaDeNacimiento = "16 diciembre 1991";
+const direccion = "La Floresta, Canelones";
+
+console.log("Nombre y apellido: ", nombre + " " + apellido);
+console.log("Mi fecha de nacimiento es: ", fechaDeNacimiento + " y mi edad es: ", edad);
+console.log("Vivo en: ", direccion);
