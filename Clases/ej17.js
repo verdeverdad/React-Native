55db127e317179270e5d4c9b85e5f7cc71fadb3b
verdeverdad <sofia.16d@gmail.com>
2025-01-16 11:56:00 -0300

    ejercicio 17

---
+++ Clases/ej17.js
@@ -0,0 +1,8 @@
+let serie = "Juego de tronos";
+let dragon1 = "Drogon"
+let dragon2 = "Rhaegal";
+let dragon3 = "Viserion";
+
+let plantilla = `Los tres dragones de la serie ${serie} se llaman: ${dragon1}, ${dragon2}, ${dragon3}.`;
+
+console.log(plantilla)
