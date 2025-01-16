f5a1f69d168f157f6722b273cfa385c94a8c46c2
verdeverdad <sofia.16d@gmail.com>
2025-01-16 13:51:18 -0300

    ejercicio 26

---
+++ Clases/ej26.js
@@ -0,0 +1,23 @@
+let nombreDelCurso ="Cocina";
+let instituto = "Utu central";
+let grado = 1;
+let numAula = 20;
+let conMatricula = true;
+let cantidadIncriptos = undefined;
+let horarios = null;
+
+console.log("Nombre del curso: " + nombreDelCurso);
+console.log("Instituto: " + instituto);
+console.log("Grado: " + grado);
+console.log("Númer del aula: " + numAula);
+console.log("Matricula: " + conMatricula);
+console.log("Cantidad de inscriptos: " + cantidadIncriptos);
+console.log("Horarios: " + horarios);
+
+console.log("Nombre del curso: " + typeof(nombreDelCurso));
+console.log("Instituto: " + typeof(instituto));
+console.log("Grado: " + typeof(grado));
+console.log("Númer del aula: " + typeof(numAula));
+console.log("Matricula: " + typeof(conMatricula));
+console.log("Cantidad de inscriptos: " + typeof(cantidadIncriptos));
+console.log("Horarios: " + typeof(horarios));
