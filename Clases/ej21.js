f7914ec56e03750c557673f8b93267bdbe343f28
verdeverdad <sofia.16d@gmail.com>
2025-01-16 12:28:09 -0300

    ejercicio 21

---
+++ Clases/ej21.js
@@ -0,0 +1,8 @@
+let edad = 33;
+let telefono = 092207574;
+let calle = "Rivera";
+let numPuerta = 2099;
+
+console.log(`Tengo ${edad} años`);
+console.log(`Mi telefono es ${telefono}`);
+console.log(`Vivo en la calle ${calle} con numero de puerta ${numPuerta}`);
