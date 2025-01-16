b883d97b0422655fab7c5fd609711e3132f3acaa
verdeverdad <sofia.16d@gmail.com>
2025-01-16 13:40:48 -0300

    ejercicio 25

---
+++ Clases/ej25.js
@@ -0,0 +1,23 @@
+let nombreDelSuperheroe ="Batman";
+let nombre = "Bruce Wayne";
+let vuela = false;
+let maneja = true;
+let vida = 50;
+let trabajo = undefined;
+let tiempoLibre = null;
+
+console.log("Nombre del superhéroe: " + nombreDelSuperheroe);
+console.log("Nombre: " + nombre);
+console.log("Vuela: " + vuela);
+console.log("Maneja: " + maneja);
+console.log("Vida: " + vida);
+console.log("Trabajo: " + trabajo);
+console.log("Tiempo libre: " + tiempoLibre);
+
+console.log("Nombre del superhéroe: " + typeof(nombreDelSuperheroe));
+console.log("Nombre: " + typeof(nombre));
+console.log("Vuela: " + typeof(vuela));
+console.log("Maneja: " + typeof(maneja));
+console.log("Vida: " + typeof(vida));
+console.log("Trabajo: " + typeof(trabajo));
+console.log("Tiempo libre: " + typeof(tiempoLibre));
