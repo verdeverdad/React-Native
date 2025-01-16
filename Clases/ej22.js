ed9877741a8452a4a04156a6e76936f744ec0302
verdeverdad <sofia.16d@gmail.com>
2025-01-16 12:46:36 -0300

    ejercicio 22

---
+++ Clases/ej22.js
@@ -0,0 +1,9 @@
+let salir = true;
+let casado = false;
+let usuarioRegistrado = false;
+let meGustaJs = true;
+
+console.log("Salir: " + salir);
+console.log("Casado : " + casado);
+console.log("Usuario registrado: " + usuarioRegistrado);
+console.log("Me gusta JS: " + meGustaJs);
