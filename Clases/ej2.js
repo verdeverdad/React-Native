52781b0c9f1f7541558151b3a48e7a71cc312edc
verdeverdad <sofia.16d@gmail.com>
2025-01-15 21:02:21 -0300

    ejercicio 2

--- Clases/ej2.js
+++ Clases/ej2.js
@@ -1,3 +1,15 @@
+var nombre;
+var apellido;
+var edad;
+var fechaDeNacimiento;
+var direccion;
+
+nombre = Sofía;
+apellido = Delmonte;
+edad = 33;
+fechaDeNacimiento = 16/12/1991;
+direccion = "La Floresta, Canelones";
+
 var nombre = Sofía;
 var apellido = Delmonte;
 var edad = 33;
