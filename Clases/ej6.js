33ade4271ce7583b2193ecef56820b1126f3f6f9
verdeverdad <sofia.16d@gmail.com>
2025-01-15 21:44:27 -0300

    ejercicio 6

---
+++ Clases/ej6.js
@@ -0,0 +1,7 @@
+var nombre = "Sofía";
+var apellido = "Delmonte";
+var edad = 33;
+var fechaDeNacimiento = "16 diciembre 1991";
+var direccion = "La Floresta, Canelones";
+
+console.log("Nombre: ", nombre, "Apellido: ", apellido, "Edad: ", edad, "Fecha de nacimiento: ", fechaDeNacimiento, "Dirección: ", direccion);
