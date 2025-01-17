e0e53f3b3070d766fc0b52bf3c0c4d51f66fd491
verdeverdad <sofia.16d@gmail.com>
2025-01-17 20:31:00 -0300

    ejercicio 28

--- Clases/ej28.js
+++ Clases/ej28.js
@@ -1,1 +1,12 @@
 let num = 9;
+
+console.log(1 * num);
+console.log(2 * num);
+console.log(3 * num);
+console.log(4 * num);
+console.log(5 * num);
+console.log(6 * num);
+console.log(7 * num);
+console.log(8 * num);
+console.log(9 * num);
+console.log(10 * num);
