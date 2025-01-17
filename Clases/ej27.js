8166b98c9f39dbbb975fc60b3824ff57c1001273
verdeverdad <sofia.16d@gmail.com>
2025-01-17 20:28:45 -0300

    ejercicio 27

--- Clases/ej27.js
+++ Clases/ej27.js
@@ -1,8 +1,19 @@
 let num1 = 10;
 let num2 = 20;
+let template = num1 + num2;
+let template1 = num1 - num2;
+let template2 = num1 * num2;
+let template3 = num1 / num2;
+let template4 = num1 % num2;
 
 console.log(num1 + num2);
 console.log(num1 - num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
 console.log(num1 % num2);
+
+console.log(template);
+console.log(template1);
+console.log(template2);
+console.log(template3);
+console.log(template4);
