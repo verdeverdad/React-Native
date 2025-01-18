f2445ac6b5beeaff4a90a981b628b78a5136ce9e
verdeverdad <sofia.16d@gmail.com>
2025-01-17 21:56:01 -0300

    ejercicio 34

---
+++ Clases/ej34.js
@@ -0,0 +1,20 @@
+let ingresos = 1000;
+let ganancias = 600;
+let impuestos = 600;
+
+console.log("objetivo de ingresos: ", ingresos >=800);
+console.log("Objetivo de impuestos: ", impuestos <= 400);
+console.log("Bono de ganancias: ", ganancias == 600);
+
+
+/*Este mes se esperan los siguientes objetivos:
+Generar al menos 800 pesos o más de ingreso
+Gastar menos de 400 pesos de impuestos
+Se Paga bono si se ganan 600 pesos de ganancias justas
+Mostrar en consola los siguientes mensajes utilizando los operadores esperados:
+Objetivo de ingresos: verdadero o falso
+Utilizar el operador mayor o igual que
+Objetivo de impuestos: verdadero o falso
+Utilizar el operador menor que
+Bono de ganancias: verdadero o falso
+Utilizar el operador de comparación simple*/
