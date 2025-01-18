bd6ca0ba4b1ee55878f1c7f05ba272c6c3a26651
verdeverdad <sofia.16d@gmail.com>
2025-01-17 23:12:41 -0300

    ejercicio 39

---
+++ Clases/ej39.js
@@ -0,0 +1,6 @@
+let nombre = "Sofia";
+let apellido = "Delmonte";
+
+
+console.log(`Mi nimbre es ${nombre} y tiene ${nombre.length} letras`);
+console.log(`Mi apellido es ${apellido} y tiene ${apellido.length} letras`);
