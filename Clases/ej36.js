a63c98addbfcf08d3a389f94bf5cc240107057df
verdeverdad <sofia.16d@gmail.com>
2025-01-17 22:07:46 -0300

    ejercicio 26

---
+++ Clases/ej36.js
@@ -0,0 +1,13 @@
+// Crear un documento con el nombre ej36.js
+// Declarar las siguientes variables numero1 y numero2
+// Asignar al numero 1 el valor 10
+// Asignar a numero2 el valor 20
+// Mostrar en consola los siguientes mensajes:
+// La variable numero1 y numero2 tienen el mismo valor: verdadero/falso
+// Las variables numero1 y numero2 son distintas: verdadero/falso
+
+let num1 = 10;
+let num2 =20;
+
+console.log(" La variable numero1 y numero2 tienen el mismo valor: ", num1 == num2);
+console.log("Las variables numero1 y numero2 son distintas: ", num1 != num2);
