7444e9d3b1114f542cce69f9291797d6df097457
verdeverdad <sofia.16d@gmail.com>
2025-01-17 23:46:12 -0300

    ejercicio 44

---
+++ Clases/ej44.js
@@ -0,0 +1,8 @@
+let texto = "HOLA";
+let primerLetra = texto.charAt(3);
+let segundaLetra = texto.charAt(2);
+let terceraLetra = texto.charAt(1);
+let cuartaLetra = texto.charAt(0);
+let resultado = primerLetra.concat(segundaLetra, terceraLetra, cuartaLetra);
+
+console.log(resultado.toLowerCase());
