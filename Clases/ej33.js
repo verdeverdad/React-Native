47583a81956a7f8985bfbd938973f777fa848e08
verdeverdad <sofia.16d@gmail.com>
2025-01-17 21:01:21 -0300

    ejercicio 33

---
+++ Clases/ej33.js
@@ -0,0 +1,20 @@
+let cantidadDeUsuarios = 100;
+
+cantidadDeUsuarios +=5;
+console.log("Cantidad de usuarios: ", cantidadDeUsuarios);
+
+cantidadDeUsuarios -=3;
+console.log("Cantidad de usuarios: ",cantidadDeUsuarios);
+
+cantidadDeUsuarios -=3;
+console.log("Cantidad de usuarios: ", cantidadDeUsuarios);
+
+cantidadDeUsuarios *=3;
+console.log("Cantidad de usuarios: ", cantidadDeUsuarios);
+
+const varones = cantidadDeUsuarios / 2;
+const mujeres = cantidadDeUsuarios / 2;
+
+console.log(`Cantidad de usuarios: ${cantidadDeUsuarios}, mujeres: ${mujeres}, varones: ${varones}`);
+
+
