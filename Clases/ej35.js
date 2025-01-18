9de6c280771c797872f31a7f5de30f8e73fea4be
verdeverdad <sofia.16d@gmail.com>
2025-01-17 22:04:13 -0300

    ejercicio 35

---
+++ Clases/ej35.js
@@ -0,0 +1,13 @@
+/*Declara la variable numero numeroDeCliente y asigna el valor 143245
+Declare la variable numeroDeClienteIngresado y asigne el valor '143245'
+Mostrar en consola si el dato ingresado y el número de cliente son iguales (comparación por valor)
+Utilizar el siguiente mensaje: El número ingresado por el cliente es correcto: true o false
+Mostrar en consola si el dato ingresado y el número de cliente son iguales (comparación por valor y tipo de dato)
+Utilizar el siguiente mensaje: El cliente ingresó un número de cliente válido: true o false*/ 
+
+let numeroDeCliente = 143245;
+let numeroDeClienteIngresado = "143245";
+
+
+console.log("El número ingresado por el cliente es correcto: ", numeroDeCliente == numeroDeClienteIngresado)
+console.log("El cliente ingresó un número de cliente válido: ", numeroDeCliente === numeroDeClienteIngresado)
