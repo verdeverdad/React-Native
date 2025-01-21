c03c6a1b8a63f97b2ae858f10a29c8e14a6bead4
verdeverdad <sofia.16d@gmail.com>
2025-01-20 22:14:51 -0300

    ejercicio 54

---
+++ Clases/ej54.js
@@ -0,0 +1,7 @@
+let usuario = 'pepe20176';
+let password = '12345';
+
+if (usuario === 'pepe2017' && password === '12345'){
+    console.log("El usuario está autenticado y puede ver el contenido del sitio")
+}
+ else {console.log("Error, no se pudo autenticar al usuario, puede intentarlo más tarde")}
