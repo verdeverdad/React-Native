515b543f0760addaeab5e3dd6013080f65daf05d
verdeverdad <sofia.16d@gmail.com>
2025-01-20 21:23:27 -0300

    ejercicio 48

--- Clases/ej48.js
+++ Clases/ej48.js
@@ -1,10 +1,10 @@
 let mensaje = '3.14 es un gran número, pero mejor es 42 que le da significado a la vida';
-let pi = parseFloat(mensaje);
+let pi = parseFloat(mensaje.slice(0, 4));
 let significadoDeLaVida = parseFloat(mensaje.slice(38, 40));
 let resultado = pi + significadoDeLaVida;
 
 console.log(pi)
 console.log(significadoDeLaVida)
 console.log(resultado);
-console.log(`${resultado} es el resultado de sumar las variables ${pi.toString()} y ${significadoDeLaVida.toString()} , concatenando los valores.`)
-
+console.log(`${resultado} es el resultado de sumar las variables ${pi.toString()} y ${significadoDeLaVida.toString()}`)
+console.log(resultado.toString() + " es el resultado de sumar las variables pi y significadoDeLaVida")
