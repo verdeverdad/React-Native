07838d1de855fb4024b5650aef08f01ca47be74b
verdeverdad <sofia.16d@gmail.com>
2025-01-20 22:07:12 -0300

    ejercicio 52

---
+++ Clases/ej52.js
@@ -0,0 +1,8 @@
+let num1 = 10;
+let num2 = 50;
+
+if (num1 >= num2) {
+    console.log("La variable num1 es más grande que la variable num2")}
+else {console.log("La variable num1 no es más grande que la variable num2")
+
+}
