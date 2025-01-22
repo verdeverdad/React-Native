69e40183256e088aeb84ff061cfd6aeb45763648
verdeverdad <sofia.16d@gmail.com>
2025-01-22 12:06:04 -0300

    ejercicio 58

---
+++ Clases/ej58.js
@@ -0,0 +1,19 @@
+let hora = 15;
+
+if (hora < 12 && hora > 5) {
+    console.log("Buen día")
+} else if ( hora > 12 && hora < 20){
+    console.log("Buenas tardes")
+} else {console.log("Buenas noches")
+
+}
+
+// Declarar la variable hora
+// Establecer un valor entre 0 y 24
+// Si la hora es menor que 12 y mayor que 5 entonces
+// Mostrar el mensaje: Buen día
+// Si la hora es mayor o igual que 12 y menor que 20 entonces
+// Mostrar el mensaje: Buenas tardes
+// Si No:
+// Mostrar el mensaje: Buenas noches
+// Cambiar el valor de la variable hora un par de veces y ejecutarlo para ver que pasa
