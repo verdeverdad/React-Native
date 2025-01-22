fa239fa8f76b00e2e782b0654d9f354a44b1071b
verdeverdad <sofia.16d@gmail.com>
2025-01-22 13:41:41 -0300

    ejercicio 65

---
+++ Clases/ej64.js
@@ -0,0 +1,7 @@
+let nombre = "So";
+
+if (nombre !== null && nombre !== undefined && nombre !== ""){
+console.log(`El nombre del usuario es ${nombre}`);
+} else {
+    console.log("Por favor ingrese un nombre");
+}
