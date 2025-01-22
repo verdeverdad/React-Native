cb1f79e6a6ac7af39351512596ab7f7d10840a24
verdeverdad <sofia.16d@gmail.com>
2025-01-22 11:50:23 -0300

    ejercicio 55

---
+++ Clases/ej55.js
@@ -0,0 +1,8 @@
+let num1 = 10;
+let num2 = 5;
+let numero = num1 >= num2 ? "La variable num1 es más grande que la variable num2" : "La variable num1 no es más grande que la variable num2";
+// if (num1 >= num2) {
+//     console.log("La variable num1 es más grande que la variable num2")}
+// else {console.log("La variable num1 no es más grande que la variable num2")
+
+// }
