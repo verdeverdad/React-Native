39f724fdbe17600c0747cef5e3995b0960b1eb36
verdeverdad <sofia.16d@gmail.com>
2025-01-22 12:00:06 -0300

    ejercicio 57

---
+++ Clases/ej57.js
@@ -0,0 +1,12 @@
+let usuario = 'pepe20176';
+let password = '12345';
+
+let autenticado = usuario === 'pepe2017' && password === '12345' ? "El usuario está autenticado y puede ver el contenido del sitio" : "Error, no se pudo autenticar al usuario, puede intentarlo más tarde";
+
+console.log(autenticado);
+
+
+// if (usuario === 'pepe2017' && password === '12345'){
+//     console.log("El usuario está autenticado y puede ver el contenido del sitio")
+// }
+//  else {console.log("Error, no se pudo autenticar al usuario, puede intentarlo más tarde")}
