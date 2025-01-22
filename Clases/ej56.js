f7d3d0727596072c8b7720f5b3502a15513e6cd1
verdeverdad <sofia.16d@gmail.com>
2025-01-22 11:56:11 -0300

    ejercicio 56

---
+++ Clases/ej56.js
@@ -0,0 +1,12 @@
+let numero = 23;
+let resultado = numero % 2 === 0 ? "El número es par" : "El número no es par";
+
+console.log(resultado);
+
+
+// if (numero % 2 === 0) {
+//   console.log("El número es par");
+// } 
+// else {
+//     console.log("El número no es impar");
+// }
