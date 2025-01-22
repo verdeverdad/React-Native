df4acc4de28fb9e7631ac6ed1e6a017dcd2cc266
verdeverdad <sofia.16d@gmail.com>
2025-01-22 12:14:19 -0300

    ejercicio 59

---
+++ Clases/ej59.js
@@ -0,0 +1,22 @@
+let num1 = 77;
+let num2 = 50;
+
+if (num1 === num2){
+    console.log("los dos numero son iguales")
+} else if (num1 > 50) {
+    console.log("La variable num1 es mayor que la variable num2")
+} else {
+     console.log("La variable num2 es mas grande que la variable num1")
+    } 
+
+
+// Declarar la variable numero1 y numero2
+// Asignar un valor numérico a cada variable declarada
+// Si la variable numero1 y numero2 son iguales entonces
+// Mostrar el siguiente mensaje en consola: Los dos números son iguales
+// Si la variable numero1 es más grande que numero2 entonces
+// Mostrar el siguiente mensaje en consola: La variable numero1 es más grande que numero2
+// Si No
+// Mostrar el siguiente mensaje en consola: La variable numero2 es más grande que numero1
+// Cambiar el valor de la variable hora un par de veces y ejecutarlo para ver que pasa
+// Probar cambiar los mensajes utilizando los valores reales de cada variable
