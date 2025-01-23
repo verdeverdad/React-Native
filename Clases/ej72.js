5dd6f89e375816fa06ce7e31d5ef73c0556aeb15
verdeverdad <sofia.16d@gmail.com>
2025-01-22 23:55:19 -0300

    ejercicio 72

---
+++ Clases/ej72.js
@@ -0,0 +1,15 @@
+let total = 14; 
+let contador = 1;
+let asteriscos = "";
+
+while (contador <= total) {
+  asteriscos += "*"; 
+  console.log(asteriscos);
+  contador++;
+}
+
+// let mensaje = 1;
+// while (mensaje <= 14) {
+//     console.log("*".repeat(mensaje ));
+//     mensaje +=2;
+// }
