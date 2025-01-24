c4d69e9425db8adaa600d784f770b3ab164f7e5b
verdeverdad <sofia.16d@gmail.com>
2025-01-24 13:47:59 -0300

    ejercicio 103

---
+++ Clases/ej103.js
@@ -0,0 +1,11 @@
+let numerosPares = function(){
+    
+    for (num = 0; num <=50; num++) {
+        console.log(num * 2)
+    }
+}
+numerosPares();
+numerosPares();
+numerosPares();
+numerosPares();
+numerosPares();
