f6dd9632ea901dc26745c20f84ccad6561b8b165
verdeverdad <sofia.16d@gmail.com>
2025-01-24 11:49:41 -0300

    ejercicio 85

---
+++ Clases/ej85.js
@@ -0,0 +1,18 @@
+
+let a = 0;
+let b = 1;
+let contador = 0;
+
+do {
+    console.log(a);
+  let c = a + b;
+  a = b;
+  b = c;
+  contador++;
+}
+while (contador < 10) {
+  let c = a + b;
+  a = b;
+  b = c;
+  contador++;
+}
