f58e2284ada8b7222c2ebef2d47c6c1d7a675669
verdeverdad <sofia.16d@gmail.com>
2025-01-24 13:35:12 -0300

    ejercicio 102

---
+++ Clases/ej102.js
@@ -0,0 +1,10 @@
+let nombre = "Sofia";
+
+function mostrarNombre(nombre) {
+    console.log("=========");
+    console.log("=="+nombre+"==")
+    console.log("=========");
+}
+
+mostrarNombre(nombre);
+mostrarNombre(nombre);
