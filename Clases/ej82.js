6427b216a8f52f75c0565ed2d912b82eb6e89c9b
verdeverdad <sofia.16d@gmail.com>
2025-01-24 11:08:43 -0300

    ejercicio 82

---
+++ Clases/ej82.js
@@ -0,0 +1,14 @@
+let total = 14; 
+let contador = 1;
+let asteriscos = "";
+
+do {
+    asteriscos += "*";
+    console.log(asteriscos);
+    contador++;
+}
+while (contador <= total) {
+  asteriscos += "*"; 
+  console.log(asteriscos);
+  contador++;
+}
