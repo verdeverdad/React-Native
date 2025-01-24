1a2c114f72dd747d7e1caec5667d1d161d9671ac
verdeverdad <sofia.16d@gmail.com>
2025-01-24 12:14:07 -0300

    ejercicio 92

---
+++ Clases/ej92.js
@@ -0,0 +1,14 @@
+let total = 14; 
+let contador = 1;
+let asteriscos = "";
+
+// while (contador <= total) {
+//   asteriscos += "*"; 
+//   console.log(asteriscos);
+//   contador++;
+// }
+
+for (contador = 1; contador <= total; contador++) {
+    asteriscos += "*"; 
+  console.log(asteriscos);
+}
