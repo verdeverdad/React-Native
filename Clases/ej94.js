2f635dc2f44ec9d69ed81935b786ac3561e830cb
verdeverdad <sofia.16d@gmail.com>
2025-01-24 12:22:38 -0300

    ejercicio 94

---
+++ Clases/ej94.js
@@ -0,0 +1,17 @@
+let total = 14; 
+let contador = 1;
+let asteriscos = "*";
+
+// console.log(asteriscos);
+// while (contador <= total) {
+//   if (contador % 2 !== 0){
+//   asteriscos += "**"; 
+//   console.log(asteriscos);}
+//   contador++;
+// }
+console.log(asteriscos);
+for (contador = 1; contador <=total; contador++) {
+    if (contador % 2 !== 0){
+          asteriscos += "**"; 
+          console.log(asteriscos);}
+}
