52beec426c73e4c75d407677ae1ab2b5419829d9
verdeverdad <sofia.16d@gmail.com>
2025-01-24 13:29:31 -0300

    ejercicio 101

---
+++ Clases/ej101.js
@@ -0,0 +1,21 @@
+let nombre = "Sofia";
+let edad = "33";
+let telefono = 92207574;
+let calle = "Rivera";
+let altura = "2020";
+let codigoPostal = 16200;
+let casado = false;
+
+function datosPersonales() {
+    console.log("Nombre: " + nombre + "\n" +
+        "Edad: " + edad + "\n" +
+         "Telefono: " + telefono + "\n" +
+         "Calle: " + calle + "\n" +
+         "Altura: " + altura + "\n" +
+         "Codigo postal: " + codigoPostal + "\n" +
+         "Casado: " + casado  
+    )
+
+}
+
+datosPersonales()
