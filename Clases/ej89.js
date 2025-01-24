bcfb81ba3b18044c14d297a29888a6e7c367268b
verdeverdad <sofia.16d@gmail.com>
2025-01-24 12:07:10 -0300

    ejercicio 89

---
+++ Clases/ej89.js
@@ -0,0 +1,13 @@
+let num = 0;
+let suma = 0;
+
+// while (num <= 1000) {
+//     suma += num;
+//     console.log(`la suma parcial de ${num} es ${suma}`);
+//     num++;
+// }
+
+for (num = 0; num <= 1000; num++) {
+    suma += num;
+    console.log(`la suma parcial de ${num} es ${suma}`);
+}
